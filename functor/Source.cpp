8377115924e2ef7db9ec237fbf9f9bd21f003dee
viktorbezkrovniy <vit123.vb@gmail.com>
2015-02-27 14:30:32 +0200

    omp

--- functor/Source.cpp
+++ functor/Source.cpp
@@ -1,5 +1,6 @@
 ﻿#include <algorithm>
 #include <iostream>
+#include <omp.h>
 
 class EvenOddFunctor {
     int even_;
@@ -15,17 +16,44 @@ public:
 };
 
 int main() {
-    EvenOddFunctor evenodd;
+   /* EvenOddFunctor evenodd;
     
     int my_list[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     evenodd = std::for_each(my_list,
                   my_list+sizeof(my_list)/sizeof(my_list[0]),
-                  evenodd);
+                  evenodd);*/
 
-    std::cout << "even " << evenodd.even_sum() << "\n";
+#pragma omp parallel
+	{
+		int i,n;
+		i = omp_get_thread_num();
+		n = omp_get_num_threads();
+		printf("I'm thread %d of %d\n",i,n);
+
+		
+		
+	}
+#pragma omp parallel for
+	{
+		for(int i = 0; i < 10000; i++)
+			printf("%d",i);
+	}
+	/*
+	int i;
+	pragma omp parallel section private(i)
+	{
+		#prgma omp section
+		  //code
+		#prgma omp section
+		  //code
+	}
+
+    /*std::cout << "even " << evenodd.even_sum() << "\n";
     std::cout << "odd " << evenodd.odd_sum() << std::endl;
 
     // вывод:
     // Сумма чётных: 30
-    // Сумма нечётных: 25
+    // Сумма нечётных: 25*/
+
+	return 0;
 }
