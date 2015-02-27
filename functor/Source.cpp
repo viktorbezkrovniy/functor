0e6443017ca539547fe16f6a02951483fada0500
viktorbezkrovniy <vit123.vb@gmail.com>
2015-02-27 10:25:56 +0200

    first

---
+++ functor/Source.cpp
@@ -0,0 +1,31 @@
+﻿#include <algorithm>
+#include <iostream>
+
+class EvenOddFunctor {
+    int even_;
+    int odd_;
+public:
+    EvenOddFunctor() : even_(0), odd_(0) {}
+    void operator()(int x) {
+        if (x%2 == 0) even_ += x;
+        else odd_ += x;
+    }
+    int even_sum() const { return even_; }
+    int odd_sum() const { return odd_; }
+};
+
+int main() {
+    EvenOddFunctor evenodd;
+    
+    int my_list[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    evenodd = std::for_each(my_list,
+                  my_list+sizeof(my_list)/sizeof(my_list[0]),
+                  evenodd);
+
+    std::cout << "even " << evenodd.even_sum() << "\n";
+    std::cout << "odd " << evenodd.odd_sum() << std::endl;
+
+    // вывод:
+    // Сумма чётных: 30
+    // Сумма нечётных: 25
+}
